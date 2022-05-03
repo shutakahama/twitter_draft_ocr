5659b2ad43d886407d014f59f7740ce49418fcc3
shutakahama <shutaka.sukehama@gmail.com>
2022-05-03 18:37:58 +0900

    fix format

--- functions.py
+++ functions.py
@@ -3,6 +3,28 @@ from PIL import Image
 from pprint import pprint
 
 
+def split_video(video_path, img_folder, interval_sec):
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        return
+    # 何frameごとにcaptureするか
+    interval = int(cap.get(cv2.CAP_PROP_FPS) * interval_sec)
+
+    idx = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        if idx % interval == 0:
+            # 画像の上端と下端は不要な情報が混ざっている可能性が高いので除去する
+            height = frame.shape[0]
+            frame = frame[int(height * 1/8):int(height * 7/8)]
+            filled_second = str(idx // interval).zfill(4)
+            cv2.imwrite(f"{img_folder}/{filled_second}.png", frame)
+        idx += 1
+
+
 def levenshtein(s1, s2):
     n, m = len(s1), len(s2)
     dp = [[0] * (m + 1) for _ in range(n + 1)]
@@ -23,8 +45,10 @@ def levenshtein(s1, s2):
 
 
 def append_string(draft_list, new_str, min_length=10, search_num=10):
+    # new_strがあまりに短すぎる場合は追加しない
     if len(new_str) < min_length:
         return draft_list
+    # 一致判定の精度を上げるため空白は除く
     new_str = new_str.replace(" ", "")
 
     # 短い方(s2)が長い方(s1)のprefixまたはsuffixに一致するかを判定する
@@ -32,21 +56,22 @@ def append_string(draft_list, new_str, min_length=10, search_num=10):
         return (levenshtein(s1[:len(s2)], s2) <= 0.5 * len(s2) or
                 levenshtein(s1[-len(s2):], s2) <= 0.5 * len(s2))
 
-    # もしnew_strと一致してより長い文章が既にdraft_listにある場合、
-    # new_strは追加するのを止める
-    # もしnew_strと一致してより短い文章が既にdraft_listにある場合、
-    # new_strを採用してdraft_listにある方を削除する
     str_to_remove = []
+    # draft_listの末尾search_num個を調査
     for s in draft_list[-search_num:]:
+        # もしnew_strと一致してより長い文章が既にdraft_listにある場合、
+        # new_strは追加するのを止める
         if len(s) >= len(new_str) and check_duplicate(s, new_str):
             return draft_list
+        # もしnew_strと一致してより短い文章が既にdraft_listにある場合、
+        # new_strを採用してdraft_listにある方を削除する
         elif len(s) < len(new_str) and check_duplicate(new_str, s):
             str_to_remove.append(s)
 
     for s in str_to_remove:
         draft_list.remove(s)
-
     draft_list.append(new_str)
+
     return draft_list
 
 
@@ -69,8 +94,8 @@ def read_strings_tesseract(img_path, draft_list, margin_ratio=1.5):
     current_draft = ''
     for i, box in enumerate(res):
         line_height = box.position[1][1] - box.position[0][1]
-        # 直前に認識した文字列の下端と、今回の文字列の上端がmargin_ratio * line_heightより
-        # 大きかった場合、別ツイートと見做す
+        # 直前に認識した文字列の下端（prev_pos）と、今回の文字列の上端が
+        # margin_ratio * line_heightより大きかった場合、別ツイートと見做す
         # append_stringを実行し、current_draftを初期化
         if prev_pos + margin_ratio * line_height <= box.position[0][1]:
             draft_list = append_string(draft_list, current_draft)
@@ -102,13 +127,15 @@ def read_strings_google_api(img_path, draft_list, margin_ratio):
         for block in page.blocks:
             prev_pos = -100
             current_draft = ''
-            # paragraphは段落または行ごとに切れていることが多い（tweet単位にはなっていない）
+            # paragraphは段落または行ごとに切れていることが多い
+            # （tweet単位にはなっていない）
             for paragraph in block.paragraphs:
                 bbox = paragraph.words[0].bounding_box.vertices
-                upper, lower = min([b.y for b in bbox]), max([b.y for b in bbox])
+                upper = min([b.y for b in bbox])
+                lower = max([b.y for b in bbox])
                 line_height = lower - upper
-                # 直前に認識した文字列の下端（prev_pos）と、今回の文字列の上端がmargin_ratio * line_heightより
-                # 大きかった場合、別ツイートと見做す
+                # 直前に認識した文字列の下端（prev_pos）と、今回の文字列の上端が
+                # margin_ratio * line_heightより大きかった場合、別ツイートと見做す
                 # append_stringを実行し、current_draftを初期化
                 if prev_pos + margin_ratio * line_height <= upper:
                     draft_list = append_string(draft_list, current_draft)
@@ -126,24 +153,3 @@ def read_strings_google_api(img_path, draft_list, margin_ratio):
             draft_list = append_string(draft_list, current_draft)
 
     return draft_list
-
-
-def split_movie(video_path, img_folder, interval_sec):
-    cap = cv2.VideoCapture(video_path)
-    if not cap.isOpened():
-        return
-    interval = int(cap.get(cv2.CAP_PROP_FPS) * interval_sec)  # 何frameごとにcaptureするか
-
-    idx = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        if idx % interval == 0:
-            # 画像の上端と下端は不要な情報が混ざっている可能性が高いので除去する
-            height = frame.shape[0]
-            frame = frame[int(height * 1/8):int(height * 7/8)]
-            filled_second = str(idx // interval).zfill(4)
-            cv2.imwrite("{}/{}.png".format(img_folder, filled_second), frame)
-        idx += 1
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import time
 
 from functions import (
-    split_movie,
+    split_video,
     read_strings_tesseract,
     read_strings_google_api
 )
@@ -29,19 +29,23 @@ def main():
     img_folder = './cropped_images'
     if not os.path.exists(img_folder):
         os.mkdir(img_folder)
-    split_movie(options.video_path, img_folder, options.frame_interval_sec)
+    split_video(options.video_path, img_folder,
+                options.frame_interval_sec)
 
     # 読み込む画像をリストアップする
-    img_list = sorted([os.path.join(img_folder, fn) for fn in os.listdir(img_folder)])
+    img_list = sorted([os.path.join(img_folder, fn)
+                       for fn in os.listdir(img_folder)])
 
     # draft_listに認識した文字列を追加していく
     draft_list = []
     for i, img in enumerate(img_list):
         print(f"reading image {i+1}/{len(img_list)}")
         if options.ocr_mode == 'tesseract':
-            draft_list = read_strings_tesseract(img, draft_list, options.margin_ratio)
+            draft_list = read_strings_tesseract(
+                img, draft_list, options.margin_ratio)
         elif options.ocr_mode == 'google_api':
-            draft_list = read_strings_google_api(img, draft_list, options.margin_ratio)
+            draft_list = read_strings_google_api(
+                img, draft_list, options.margin_ratio)
 
     # 認識した結果を書き出す
     draft_json = {i: e for i, e in enumerate(draft_list)}
